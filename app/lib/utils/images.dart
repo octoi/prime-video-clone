966670484a49caee3bb02fef808ace123b36da77
fadhilsaheer <fadhilsaheer@gmail.com>
2021-10-15 04:35:34 +0530

    latest movies images

--- app/lib/utils/images.dart
+++ app/lib/utils/images.dart
@@ -6,13 +6,8 @@ const List<String> topImages = [
 
 const List<String> latestMoviesImages = [
   'https://st1.bgr.in/wp-content/uploads/2021/07/Black-Widow-release-date-in-India.jpg',
-  'https://st1.bgr.in/wp-content/uploads/2021/07/Black-Widow-release-date-in-India.jpg',
-  'https://st1.bgr.in/wp-content/uploads/2021/07/Black-Widow-release-date-in-India.jpg',
-  'https://st1.bgr.in/wp-content/uploads/2021/07/Black-Widow-release-date-in-India.jpg',
-  'https://st1.bgr.in/wp-content/uploads/2021/07/Black-Widow-release-date-in-India.jpg',
-  'https://st1.bgr.in/wp-content/uploads/2021/07/Black-Widow-release-date-in-India.jpg',
-  'https://st1.bgr.in/wp-content/uploads/2021/07/Black-Widow-release-date-in-India.jpg',
-  'https://st1.bgr.in/wp-content/uploads/2021/07/Black-Widow-release-date-in-India.jpg',
-  'https://st1.bgr.in/wp-content/uploads/2021/07/Black-Widow-release-date-in-India.jpg',
-  'https://st1.bgr.in/wp-content/uploads/2021/07/Black-Widow-release-date-in-India.jpg',
+  'https://www.thelivemirror.com/wp-content/uploads/2021/09/eternals.jpg',
+  'https://static1.colliderimages.com/wordpress/wp-content/uploads/2021/08/what-if-sw-crossover-episode-social.jpg',
+  'https://i.ytimg.com/vi/COGg-Vjauwo/maxresdefault.jpg',
+  'https://cdn.flickeringmyth.com/wp-content/uploads/2020/01/hawkeye.png',
 ];
