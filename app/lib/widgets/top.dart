31119cc59f781b65fd1fd3e0dd6f22395bfaef9c
fadhilsaheer <fadhilsaheer@gmail.com>
2021-10-14 20:30:23 +0530

    building: top portion

---
+++ app/lib/widgets/top.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Top extends StatelessWidget {
+  const Top({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Row(
+        children: [],
+      ),
+    );
+  }
+}
