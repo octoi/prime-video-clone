24287d7d3cf674655be0bdd134831bfb69dff6f1
fadhilsaheer <fadhilsaheer@gmail.com>
2021-10-15 04:10:29 +0530

    top section

--- app/lib/screens/home_screen.dart
+++ app/lib/screens/home_screen.dart
@@ -7,18 +7,32 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: appBgColor,
-      appBar: AppBar(
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
         backgroundColor: appBgColor,
-        elevation: 0.0,
-        title: const Text('prime video'),
-        centerTitle: true,
-      ),
-      body: SingleChildScrollView(
-        physics: const BouncingScrollPhysics(),
-        child: Column(
-          children: const [Top()],
+        appBar: AppBar(
+          backgroundColor: appBgColor,
+          elevation: 0.0,
+          title: const Text('prime video'),
+          centerTitle: true,
+          bottom: const TabBar(
+            indicatorColor: Colors.white,
+            tabs: [
+              Tab(text: 'Home'),
+              Tab(text: 'TV Shows'),
+              Tab(text: 'Movies'),
+              Tab(text: 'Kids'),
+            ],
+          ),
+        ),
+        body: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Column(
+            children: const [
+              Top(),
+            ],
+          ),
         ),
       ),
     );
--- app/lib/widgets/top.dart
+++ app/lib/widgets/top.dart
@@ -1,3 +1,4 @@
+import 'package:app/utils/images.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -7,8 +8,29 @@ class Top extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
       child: Row(
-        children: [],
+        children: topImages.map((image) => TopItem(image: image)).toList(),
+      ),
+    );
+  }
+}
+
+class TopItem extends StatelessWidget {
+  final String image;
+
+  const TopItem({Key? key, required this.image}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 220.0,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: NetworkImage(image),
+          fit: BoxFit.cover,
+        ),
       ),
     );
   }
